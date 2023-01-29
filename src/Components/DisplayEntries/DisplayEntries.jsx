d3a21947e280e0071c635062ff3910db874566b1
Lady Meek <ladyjmeek@gmail.com>
2023-01-29 11:32:18 -0800

    created display entries component and instantiated component on app component

---
+++ src/Components/DisplayEntries/DisplayEntries.jsx
@@ -0,0 +1,25 @@
+const DisplayEntries = (props) => {
+    return (       
+        <table>
+            <thread>
+            <tr>
+                <th>Entry Number</th>
+                <th>Weight</th>
+                <th>Date</th>
+            </tr>
+            </thread>
+            <tbody> 
+            {props.parentEntries.map((entry, index) => {
+                return (
+                <tr>
+                    <td>{index + 1} </td>
+                    <td>{entry.weight}</td>
+                    <td>{entry.date}</td>
+                </tr>
+                );
+            })}
+            </tbody>
+        </table> );
+}
+ 
+export default DisplayEntries;
